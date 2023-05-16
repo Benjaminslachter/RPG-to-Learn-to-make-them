9deab64b0fc890b91725b8ed31db25de089dfb5e
Benjamin Slachter <benjamin.slachter@gmail.com>
2023-05-16 09:00:52 -0400

    It should work soon... maybe.

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const speed = 100
 var current_dir = "none"
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	player_movement()
 
 func player_movement():
@@ -14,17 +14,21 @@ func player_movement():
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
+		play_anim(1)
 		velocity.y = speed
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_up"):
 		current_dir = "up"
+		play_anim(1)
 		velocity.y = -speed
 		velocity.x = 0
 	else:
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 	
@@ -36,25 +40,21 @@ func play_anim(movement):
 	var anim = $AnimatedSprite2D
 	
 	if dir == "right":
-		anim.flip.h = false
 		if movement == 1:
 			anim.play("Walk_Side")
 		elif movement == 0:
 			anim.play("Idle_Side")
 	if dir == "left":
-		anim.flip.h = true
 		if movement == 1:
 			anim.play("Walk_Side")
 		elif movement == 0:
 			anim.play("Idle_Side")
 	if dir == "down":
-		anim.flip.h = false
 		if movement == 1:
-			anim.play("Walk_Down")
+			anim.play("Walk_Front")
 		elif movement == 0:
-			anim.play("Idle_Down")
+			anim.play("Idle_Front")
 	if dir == "up":
-		anim.flip.h = false
 		if movement == 1:
 			anim.play("Walk_Up")
 		elif movement == 0:
